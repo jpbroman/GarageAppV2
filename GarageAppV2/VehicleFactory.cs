310960cc4895d0d2d8a2c516ed895b895e523eb4
Peter Broman <jp.broman@telia.com>
2026-05-21 00:40:11 +0200

    Cleaning up in VehicleFactory

--- GarageAppV2/VehicleFactory.cs
+++ GarageAppV2/VehicleFactory.cs
@@ -3,22 +3,6 @@ using System.Dynamic;
 namespace GarageApp;
 public class VehicleFactory
 {   
- 
-    // public static Vehicle? CreateVehicle(IUI ui, string type, string make, string color, string regNumber, int numSeats)
-    // {
-    //     Vehicle? vehicle = null;
-    //     // base data
-    //     Vehicle.VehicleTypeE vehicleType = Enum.TryParse<Vehicle.VehicleTypeE>(type, true, out var vt) ? vt : Vehicle.VehicleTypeE.Unknown;
-    //     try {
-    //         vehicle = new Vehicle(vehicleType, make, color, regNumber, numSeats);
-    //     }
-    //     catch (ArgumentException ex)
-    //     {
-    //         Console.WriteLine(ex.Message);
-    //         return null;
-    //     }
-    // }
-
     public static Vehicle? CreateVehicleFromData(string type, string data)
     {
         int seats = 0;
@@ -74,71 +58,4 @@ public class VehicleFactory
                 throw new ArgumentException($"Unknown vehicle type: {type}");
         }
     }
-    // private static Car CreateCar(Vehicle vehicle)   
-    // {
-    //      prompt = ($"Enter car type ({string.Join("/", Enum.GetNames<Car.CarTypeE>())}): ");
-    //      Car.CarTypeE carType = Enum.Parse<Car.CarTypeE>(Utils.SafeInput(prompt), true);
-    //     // prompt = $"Enter transmission type (Automatic/Manual): ";
-    // //    Car.TransmissionE transmission = Enum.Parse<Car.TransmissionE>(Utils.SafeInput(prompt), true);
-    //     Car car = new Car(vehicle.Make, vehicle.Color, vehicle.RegNumber, type, trans);
-    //     return car;
-    // }
-
-    // private static Motorcycle CreateMotorcycle(Vehicle vehicle)
-    // {
-    //     prompt = ($"Enter motorcycle type ({string.Join("/", Enum.GetNames<Motorcycle.McTypeE>())}): ");
-    //     Motorcycle.McTypeE mcType = Enum.Parse<Motorcycle.McTypeE>(Utils.SafeInput(prompt), true);
-    //     prompt = "Enter engine type (TwoStroke/FourStroke): ";
-    //     Motorcycle.EngineTypeE engineType = Enum.Parse<Motorcycle.EngineTypeE>(Utils.SafeInput(prompt), true);
-    //     Motorcycle motorcycle = new Motorcycle(vehicle.Make, vehicle.Color, vehicle.RegNumber, mcType, engineType);
-    //     return motorcycle;
-    // }
-
-    // private static Bus CreateBus(Vehicle vehicle)
-    // {
-    //     prompt = ($"Enter bus type ({string.Join("/", Enum.GetNames<Bus.BusTypeE>())}): ");
-    //     Bus.BusTypeE busType = Enum.Parse<Bus.BusTypeE>(Utils.SafeInput(prompt), true);
-    //     prompt = "Enter seating capacity: ";
-    //     int seatingCapacity;
-    //     while (!int.TryParse(Utils.SafeInput(prompt), out seatingCapacity))
-    //     {
-    //         Console.WriteLine("Enter a valid integer for seating capacity:");
-    //     }
-    //     Bus bus = new Bus(vehicle.Make, vehicle.Color, vehicle.RegNumber, busType, seatingCapacity);
-    //     return bus;
-    // }
-
-    // private static Airplane CreateAirplane(Vehicle vehicle)
-    // {
-    //     prompt = ($"Enter airplane type ({string.Join("/", Enum.GetNames<Airplane.AirplaneTypeE>())}): ");
-    //     Airplane.AirplaneTypeE airplaneType = Enum.Parse<Airplane.AirplaneTypeE>(Utils.SafeInput(prompt), true);
-    //     prompt = "Enter number of engines: ";
-    //     int numEngines;
-    //     while (!int.TryParse(Utils.SafeInput(prompt), out numEngines))
-    //     {
-    //         Console.WriteLine("Enter a valid integer for number of engines:");
-    //     }
-    //     prompt = "Enter number of seats: ";
-    //     int numSeats;
-    //     while (!int.TryParse(Utils.SafeInput(prompt), out numSeats))
-    //     {
-    //         Console.WriteLine("Enter a valid integer for number of seats:");
-    //     }
-    //     Airplane airplane = new Airplane(vehicle.Make, vehicle.Color, vehicle.RegNumber, airplaneType, numEngines, numSeats);
-    //     return airplane;
-    // }
-
-    // private static Boat CreateBoat(Vehicle vehicle)
-    // {
-    //     prompt = ($"Enter boat type ({string.Join("/", Enum.GetNames<Boat.BoatTypeE>())}): ");
-    //     Boat.BoatTypeE boatType = Enum.Parse<Boat.BoatTypeE>(Utils.SafeInput(prompt), true);
-    //     prompt = "Enter length (ft): ";
-    //     double length;
-    //     while (!double.TryParse(Utils.SafeInput(prompt), out length))
-    //     {
-    //         Console.WriteLine("Enter a valid number for length:");
-    //     }
-    //     Boat boat = new Boat(vehicle.Make, vehicle.Color, vehicle.RegNumber, boatType, length);
-    //     return boat;
-    // }
-}   
+ }   
